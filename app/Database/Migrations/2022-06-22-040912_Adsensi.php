90605f4189a0bfa0abbb3cf60ca0c53faf6a0776
KangSunat <92769271+kangsunat@users.noreply.github.com>
2022-06-22 11:18:59 +0700

    create git ignore

---
+++ app/Database/Migrations/2022-06-22-040912_Adsensi.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Adsensi extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id_absensi' => ['type' => 'int', 'auto_increment' => true],
+			'id_user' => ['type' => 'int', 'constraint' => '16'],
+			'in' => ['type' => 'time'],
+			'out' => ['type' => 'time'],
+			'created_at' => ['type' => 'date'],
+		]);
+
+		$this->forge->addKey('id_absensi', true);
+		$this->forge->createTable('absensi');
+	}
+
+	public function down()
+	{
+		//
+	}
+}
